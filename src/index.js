161fc8d91116b7e3399769ea565300934bc0ebe6
Yelena Strizhakova <e.strizhakova@gmail.com>
2020-12-24 15:57:41 +0500

    routing

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom';
 import './pages/index.css';
 import './pages/initiation.css';
 import App from './components/App';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
